ef7c1ab02a97ea076eb7cfe42c56364d2a55df74
Matis <matisrod91@gmail.com>
2023-04-11 22:37:24 +0200

    mise a jout du main permettant maintenant de visualisé sur la carte les ville sélectionner par une requete sql

--- main/src/main.py
+++ main/src/main.py
@@ -1,6 +1,7 @@
 from dataToDB.creationBD import creationBD
 from dataToDB.insertionBD import toutInserer, insererPremieresDonnees ,insererCoords, mesDonnes, insertionPopulation
 from dataToDB.query import query
+from graphics.CarteFrance import AffichageCarteFrance
 
 TESTER = True # TRUE = JE FAIS DES TESTS
 CREER = True # JE CRÉER UNE DB
@@ -12,7 +13,6 @@ if __name__ == "__main__":
         #création d'une base de données de test :
         creation = creationBD(TESTER)
         
-    
 
     if INSERER:
         #extraction données des fichiers :
@@ -27,7 +27,23 @@ if __name__ == "__main__":
         
         
     if REQUETER:
-        #votre requete :
-        # exemple : on donne le nom de toute les villes avec leur nombred'habitant en 2020
-        query(TESTER, "select lib, nbHab from commune c, population p WHERE c.codeG=p.codeG AND annees = 2020")
+        #query(TESTER, "select lib, nbhab from commune c, population p WHERE (c.codeG = p.codeG) AND (lib LIKE 'ajaccio%' OR lib LIKE 'bastia%') AND annees = 2020")
+
         
+        #votre requete :
+        reponse = query(TESTER, "select lib, lat, long from commune WHERE dept LIKE '91'")
+        data = {reponse[x][0] : [reponse[x][y] for y in range(1, len(reponse[0]))] for x in range(len(reponse))}
+        maMarte = AffichageCarteFrance(data)
+        maMarte.creationCarte()
+
+
+        # exemple : donne les extrems de la france métropolitaine, la corse exclue : (fonctionne si on a toutes les données des coords dans notre base)
+        reponse = query(TESTER, '''select max(lat), min(lat), max(long), min(long) from commune 
+                         WHERE (codeG LIKE '01_%' OR codeG LIKE '1_%' 
+                                                 OR codeG LIKE '2_%' OR codeG LIKE '3_%' OR codeG LIKE '4_%' 
+                                                 OR codeG LIKE '5_%' OR codeG LIKE '6_%' OR codeG LIKE '7_%' 
+                                                 OR codeG LIKE '8_%' OR codeG LIKE '91_%'
+                                                 OR codeG LIKE '92_%' OR codeG LIKE '93_%'
+                                                 OR codeG LIKE '94_%' OR codeG LIKE '95_%')
+                                                 AND NOT --on exclue ici la Corse
+                                                 (codeG LIKE '2A_%' OR codeG LIKE '2B_%')''')
