5156d914f4fbb8190bdb1dd3f6ebebba2f05ccd2
Matis <matisrod91@gmail.com>
2023-04-13 22:24:13 +0200

    ajout du fichier conrtenant les erreurs graphiques

---
+++ main/src/graphics/erreurs.py
@@ -0,0 +1,6 @@
+def dicoErreur():
+    erreurs = {
+        0 : "Vous n'avez sélectionner aucunes requêtes !",
+        1 : "Vous n'avez pas sélectionné de base de données à exécuter !"
+    }
+    return erreurs
