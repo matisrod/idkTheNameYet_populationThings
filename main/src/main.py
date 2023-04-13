2c1b4e685f9778b895afbc83857945605bac3805
Matis <matisrod91@gmail.com>
2023-04-13 18:05:16 +0200

    mise a jourdu main en conséquence de la fenetre graphique

--- main/src/main.py
+++ main/src/main.py
@@ -2,28 +2,28 @@ from dataToDB.creationBD import creationBD
 from dataToDB.insertionBD import toutInserer, insererPremieresDonnees ,insererCoords, mesDonnes, insertionPopulation
 from dataToDB.query import query
 from graphics.CarteFrance import AffichageCarteFrance
+from graphics.FenetreDesParametres import mainfenetre
 
-TESTER = True # TRUE = JE FAIS DES TESTS
-CREER = True # JE CRÉER UNE DB
-INSERER = True # J'INSERE DANS MA DB
+TESTER = False # TRUE = JE FAIS DES TESTS
+CREER = False # JE CRÉER UNE DB
+INSERER = False # J'INSERE DANS MA DB
 REQUETER = True # je fais une requete a ma DB
 
 if __name__ == "__main__":
+    mainfenetre()
+    '''
     if CREER:
         #création d'une base de données de test :
         creation = creationBD(TESTER)
         
 
     if INSERER:
-        #extraction données des fichiers :
-        donneesBasePop, donneesLaposteHexa = mesDonnes(TESTER)
-
         #inserer toutes les info : codeGeographique, region, departement, libelleGeo, latitude, longitude
 
         #insererPremieresDonnees(donneesBasePop, TESTER)
         #insererCoords(donneesLaposteHexa, TESTER)
         #insertionPopulation(TESTER, donneesBasePop)
-        toutInserer(donneesBasePop, donneesLaposteHexa, TESTER)
+        toutInserer(TESTER)
         
         
     if REQUETER:
@@ -35,15 +35,4 @@ if __name__ == "__main__":
         data = {reponse[x][0] : [reponse[x][y] for y in range(1, len(reponse[0]))] for x in range(len(reponse))}
         maMarte = AffichageCarteFrance(data)
         maMarte.creationCarte()
-
-
-        # exemple : donne les extrems de la france métropolitaine, la corse exclue : (fonctionne si on a toutes les données des coords dans notre base)
-        reponse = query(TESTER, '''select max(lat), min(lat), max(long), min(long) from commune 
-                         WHERE (codeG LIKE '01_%' OR codeG LIKE '1_%' 
-                                                 OR codeG LIKE '2_%' OR codeG LIKE '3_%' OR codeG LIKE '4_%' 
-                                                 OR codeG LIKE '5_%' OR codeG LIKE '6_%' OR codeG LIKE '7_%' 
-                                                 OR codeG LIKE '8_%' OR codeG LIKE '91_%'
-                                                 OR codeG LIKE '92_%' OR codeG LIKE '93_%'
-                                                 OR codeG LIKE '94_%' OR codeG LIKE '95_%')
-                                                 AND NOT --on exclue ici la Corse
-                                                 (codeG LIKE '2A_%' OR codeG LIKE '2B_%')''')
+    '''
